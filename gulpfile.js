efc178f1d56150f9828809955e18ddf10ed7e4fc
MD ILIAS <mdilias41@gmail.com>
2019-04-01 21:19:14 +0530

    live

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,16 @@ const sass        = require('gulp-sass');
 
 // Compile Sass & Inject Into Browser
 gulp.task('sass', function() {
-    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+    return gulp.src(['node_modules/bootstrap/dist/css/bootstrap.min.css', 'docs/scss/*.scss'])
         .pipe(sass())
-        .pipe(gulp.dest("src/css"))
+        .pipe(gulp.dest("docs/css"))
         .pipe(browserSync.stream());
 });
 
-// Move JS Files to src/js
+// Move JS Files to docs/js
 gulp.task('js', function() {
     return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js','node_modules/popper.js/dist/umd/popper.min.js'])
-        .pipe(gulp.dest("src/js"))
+        .pipe(gulp.dest("docs/js"))
         .pipe(browserSync.stream());
 });
 
@@ -21,23 +21,23 @@ gulp.task('js', function() {
 gulp.task('serve', ['sass'], function() {
 
     browserSync.init({
-        server: "./src"  
+        server: "./docs"
     });
 
-    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'docs/scss/*.scss'], ['sass']);
+    gulp.watch("docs/*.html").on('change', browserSync.reload);
 });
 
-// Move Fonts to src/fonts
+// Move Fonts to docs/fonts
 gulp.task('fonts', function() {
   return gulp.src('node_modules/font-awesome/fonts/*')
-    .pipe(gulp.dest('src/fonts'))
+    .pipe(gulp.dest('docs/fonts'))
 })
 
-// Move Font Awesome CSS to src/css
+// Move Font Awesome CSS to docs/css
 gulp.task('fa', function() {
   return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
-    .pipe(gulp.dest('src/css'))
+    .pipe(gulp.dest('docs/css'))
 })
 
 gulp.task('default', ['js','serve', 'fa', 'fonts']);
